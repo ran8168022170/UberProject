abe3a024d044f1bfbc0a51259916ce711d3f379e
Ranjeet Kanu <ranjeetkanu77@gmail.com>
2026-07-15 02:10:29 +0530

    /users/register endpoint created

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,18 @@ dotenv.config();
 const express = require("express");
 const app = express();
 
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.route.js");
+const cookieParser = require("cookie-parser");
+//app.use(cors());
+
+connectToDb();
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello worldqq");
 });
+app.use("/users", userRoutes);
 
 module.exports = app;
